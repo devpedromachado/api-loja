1f0d09464ddd77a9e550061aaeb92c80191dfbc8
devpedromachado <pedrohdmachado@gmail.com>
2025-02-14 16:50:47 -0300

    env

--- rotas.js
+++ rotas.js
@@ -11,7 +11,7 @@ client.connect()
     .catch(err => console.error("Erro ao conectar:", err));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
@@ -186,7 +186,7 @@ app.delete('/produtos', async(req, res) => {
 }); 
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando na porta${port}`);
 });
 
 
